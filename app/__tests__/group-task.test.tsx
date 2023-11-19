fb809ac6b0f49ba9a76be3b2c188ede684b8a4ac
plumest <plumest.chayathon@gmail.com>
2023-11-20 02:19:01 +0700

    Add test about delete task

--- app/__tests__/group-task.test.tsx
+++ app/__tests__/group-task.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { describe, expect, test } from "@jest/globals";
-import { groupTasks } from "../_actions/group-task-action";
+import { deleteTask, groupTasks } from "../_actions/group-task-action";
 import { IGroupedTodo, ITodo } from "@/lib/interfaces";
 
 describe("grouping task", () => {
@@ -26,7 +26,7 @@ describe("grouping task", () => {
     ];
   });
 
-  test("When add new date task, It should create new date", () => {
+  test("When add new date task, It should create new date.", () => {
     newTasks = [
       {
         id: "2",
@@ -56,7 +56,7 @@ describe("grouping task", () => {
     ]);
   });
 
-  test("When add existing date task, It should append task to existing date", () => {
+  test("When add existing date task, It should append task to existing date.", () => {
     newTasks = [
       {
         id: "3",
@@ -78,6 +78,70 @@ describe("grouping task", () => {
             createdAt: "2021-08-01T00:00:00.000Z",
           },
           ...newTasks,
+        ],
+      },
+    ]);
+  });
+
+  test("When delete last task for given date, It should delete that date object.", () => {
+    expect(deleteTask(previousGroups, "1", "2021-08-01")).toEqual([]);
+  });
+
+  test("When delete task, It should return remove only that task.", () => {
+    const newGroups = groupTasks(previousGroups, [
+      {
+        id: "3",
+        title: "test",
+        description: "test",
+        status: "TODO",
+        createdAt: "2021-08-01T00:00:00.000Z",
+      },
+    ]);
+    expect(deleteTask(newGroups, "1", "2021-08-01")).toEqual([
+      {
+        date: "2021-08-01",
+        tasks: [
+          {
+            id: "3",
+            title: "test",
+            description: "test",
+            status: "TODO",
+            createdAt: "2021-08-01T00:00:00.000Z",
+          },
+        ],
+      },
+    ]);
+  });
+
+  test("When delete non existing task, It should return previous value.", () => {
+    expect(deleteTask(previousGroups, "100", "2021-08-01")).toEqual([
+      {
+        date: "2021-08-01",
+        tasks: [
+          {
+            id: "1",
+            title: "test",
+            description: "test",
+            status: "TODO",
+            createdAt: "2021-08-01T00:00:00.000Z",
+          },
+        ],
+      },
+    ]);
+  });
+
+  test("When delete with non existing date, It should return previous value.", () => {
+    expect(deleteTask(previousGroups, "1", "2021-08-10")).toEqual([
+      {
+        date: "2021-08-01",
+        tasks: [
+          {
+            id: "1",
+            title: "test",
+            description: "test",
+            status: "TODO",
+            createdAt: "2021-08-01T00:00:00.000Z",
+          },
         ],
       },
     ]);
